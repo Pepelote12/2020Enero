866cc68db1d7d5cb4fe8492ade8db8f4a064f754
Luis Gerardo Navarrete Silva <60372172+Pepelote12@users.noreply.github.com>
2020-02-21 20:11:45 -0800

    Version 1 ACTUALIZADO
    No se tenia que imprimir con un metodo

--- Setup/pelicula/Program.cs
+++ Setup/pelicula/Program.cs
@@ -6,46 +6,17 @@ namespace Pelicula
     {
         class Pelicula
         {
-            private string titulo, pais, director;
-            private Int16 año;
+            public string titulo, pais, director;
+            public Int16 año;
 
-            //public void Imprimir()
-            //{
-            //    Console.WriteLine("La La Land");
-            //    Console.WriteLine(2016);
-            //    Console.WriteLine("La forma del agua");
-            //    Console.WriteLine(2017);
-            //}
-
-            public void SetTitulo(string titulo)
-            {
-                this.titulo=titulo;
-            }
-            public void SetAño(Int16 año)
-            {
-                this.año=año;
-            }
-            public string GetTitulo()
-            {
-                return this.titulo;
-            }
-
-            public Int16 GetAño()
-            {
-                return this.año;
-            }
 
             static void Main(string[] args)
             {
                 Pelicula p1 = new Pelicula();
-                p1.SetTitulo("La La Land");
-                p1.SetAño(2016);
-                Console.WriteLine("{0},{1}",p1.GetTitulo(), p1.GetAño());
+                Console.WriteLine("La La Land, 2016");
                 
                 Pelicula p2 = new Pelicula();
-                p2.SetTitulo("La forma del agua");
-                p2.SetAño(2017);
-                Console.WriteLine("{0},{1}",p2.GetTitulo(), p2.GetAño());
+                Console.WriteLine("La forma del agua, 2017");
             }
         }
     }
