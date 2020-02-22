5365aaaa5464db3e13ab46254380301e03253af9
Luis Gerardo Navarrete Silva <60372172+Pepelote12@users.noreply.github.com>
2020-02-21 19:41:51 -0800

    Version 2

--- Setup/pelicula/Program.cs
+++ Setup/pelicula/Program.cs
@@ -6,23 +6,46 @@ namespace Pelicula
     {
         class Pelicula
         {
-            public string titulo, pais, director;
-            public int año;
+            private string titulo, pais, director;
+            private Int16 año;
 
-            public void Imprimir()
+            //public void Imprimir()
+            //{
+            //    Console.WriteLine("La La Land");
+            //    Console.WriteLine(2016);
+            //    Console.WriteLine("La forma del agua");
+            //    Console.WriteLine(2017);
+            //}
+
+            public void SetTitulo(string titulo)
             {
-                Console.WriteLine("La La Land");
-                Console.WriteLine(2016);
-                Console.WriteLine("La forma del agua");
-                Console.WriteLine(2017);
+                this.titulo=titulo;
+            }
+            public void SetAño(Int16 año)
+            {
+                this.año=año;
+            }
+            public string GetTitulo()
+            {
+                return this.titulo;
             }
 
+            public Int16 GetAño()
+            {
+                return this.año;
+            }
 
             static void Main(string[] args)
             {
                 Pelicula p1 = new Pelicula();
-                p1.Imprimir();
+                p1.SetTitulo("La La Land");
+                p1.SetAño(2016);
+                Console.WriteLine("{0},{1}",p1.GetTitulo(), p1.GetAño());
+                
                 Pelicula p2 = new Pelicula();
+                p2.SetTitulo("La forma del agua");
+                p2.SetAño(2017);
+                Console.WriteLine("{0},{1}",p2.GetTitulo(), p2.GetAño());
             }
         }
     }
