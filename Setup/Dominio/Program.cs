f67971a640fb4fa087a6cf5a09cee211aea13452
Pepelote12 <60372172+Pepelote12@users.noreply.github.com>
2020-03-11 11:54:20 -0800

    Suma con ref

---
+++ Setup/Dominio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dominio
+{
+    class Dominio
+    {
+        int Espacio1, Espacion2;
+        public static void suma(ref int x, ref int y)
+        {
+            z=x+y;
+        }
+        static void Main(string[] args)
+        {
+            int x= 10; int y=12;
+            Dominio.suma(ref x, ref y)
+            Console.WriteLine(z);
+        }
+    }
+}
